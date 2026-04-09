5aa3399b3f48929d8c2544648e2121edf5e1d16c
mudasir687abbas <mudasir687abbas@gamil.com>
2026-04-10 01:27:26 +0500

    Change server api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'http://localhost:5000'
+      '/api':'https://simple-login-logout-portal-backend.vercel.app/'
     }
   },
   plugins: [react()],
