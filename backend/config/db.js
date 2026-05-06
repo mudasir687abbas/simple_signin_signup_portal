054ab60f69ad77582204e21adb92cd77acaf6ffa
mudasir687abbas <mudasir687abbas@gamil.com>
2026-05-07 01:39:09 +0500

    MongoDB Atlas Issue

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import  mongoose from "mongoose";
 
 // Replace with your MongoDB connection string
 // const MONGO_URI = "mongodb+srv://mudasir687abbas_db_user:Risadumm786$@mern-mudasir.iljvu9x.mongodb.net/?appName=mern-mudasir";
-const MONGO_URI = "mongodb://mudasir687abbas_db_user:risadumm786$@ac-hxmwou3-shard-00-00.iljvu9x.mongodb.net:27017,ac-hxmwou3-shard-00-01.iljvu9x.mongodb.net:27017,ac-hxmwou3-shard-00-02.iljvu9x.mongodb.net:27017/?ssl=true&replicaSet=atlas-11umx3-shard-0&authSource=admin&appName=mern-mudasir"
+const MONGO_URI = "mongodb://mudasir687abbas_db_user:Risadumm786$@ac-i9hprga-shard-00-00.sv3ndmo.mongodb.net:27017,ac-i9hprga-shard-00-01.sv3ndmo.mongodb.net:27017,ac-i9hprga-shard-00-02.sv3ndmo.mongodb.net:27017/?ssl=true&replicaSet=atlas-yrfa3c-shard-0&authSource=admin&appName=portalDB";
 let con = null;
 // Function to connect to MongoDB
 const connectDB = async () => {
