c6e8e71734702de0ab2eb0aadf4fe7bc9b040293
mudasir687abbas <mudasir687abbas@gamil.com>
2026-05-07 02:14:44 +0500

    Adding cors

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import { middlewareError } from './middleware/middlewareError.js';
 const app = express();
 
 
-// app.use(cors({ origin: 'http://localhost:5173'}));
+app.use(cors());
 app.use(express.json());
 
 
